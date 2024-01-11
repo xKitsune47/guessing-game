4ecb0bb41ecc78c67c11cf5b369617d520dda8d9
xKitsune47 <franek.cy@wp.pl>
2024-01-12 00:35:46 +0100

    added play again button creation, input locking after winning, generating random number

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function answerChecking(){
     if (!document.getElementById('numberInput').value){
         document.getElementById('answer').innerHTML = 'Write a number';
     } else{
-        if (guessedNumber === 3){
+        if (guessedNumber === generatedNumber){
             document.getElementById('answer').innerHTML = 'correct!';
-        } else if (guessedNumber < 3) {
+            lockInputs();
+            createPlayAgainButton();
+        } else if (guessedNumber < generatedNumber) {
             document.getElementById('answer').innerHTML = 'incorrect, try higher';
-        } else if (guessedNumber > 3) {
+        } else if (guessedNumber > generatedNumber) {
             document.getElementById('answer').innerHTML = 'incorrect, try lower';
         } else {
             alert('Something unexpected happened! Please refresh the site.')
@@ -24,12 +26,35 @@ function answerChecking(){
     return;
 }
 
+function lockInputs(){
+    document.getElementById('numberInput').setAttributeNode(document.createAttribute('disabled'));
+    document.getElementById('submitAnswer').setAttributeNode(document.createAttribute('disabled'));
+}
+
+function createPlayAgainButton(){
+    const buttonDestination = document.getElementById('answerBlock');
+    const createButton = document.createElement('input');
+    const parentElement = document.getElementsByTagName('span')[1];
+    createButton.setAttribute('type', 'submit');
+    createButton.setAttribute('onclick', 'generatingNumber();');
+    createButton.setAttribute('value', 'Play again!');
+    createButton.setAttribute('id', 'playAgainButton');
+    parentElement.append(createButton);
+    return;
+}
+
 function onPressedEnter(){
     if (event.keyCode === 13 || event.key === "ENTER"){
         answerChecking();
         return;
     }
 }
+
+function generatingNumber(){
+    return Math.floor(Math.random() * 10)+1;
+}
+let generatedNumber = generatingNumber();
+console.log(generatedNumber);
 
 const siteWidth = 1280;
 let scale = screen.width /siteWidth;
