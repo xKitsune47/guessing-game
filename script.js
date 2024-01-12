591699d60989db126e52d9c33ecabd20fbdd8314
xKitsune47 <franek.cy@wp.pl>
2024-01-12 16:07:51 +0100

    finished project, havent adapted it for mobile use

--- script.js
+++ script.js
@@ -3,28 +3,33 @@
 function answerChecking(){
     let guessedNumber = parseInt(document.getElementById('numberInput').value);
     if (!document.getElementById('numberInput').value){
-        document.getElementById('answer').innerHTML = 'Write a number';
+        document.getElementById('answer').innerHTML = 'write a number';
     } else{
         if (guessedNumber > 50 || guessedNumber < 1){
             document.getElementById('answer').innerHTML = 'number not in guessing range';
         } else {
             if (listOfGuessedNumbers.includes(guessedNumber)){
-                document.getElementById('answer').innerHTML = `You've already tried number ${guessedNumber}`;
+                document.getElementById('answer').innerHTML = `you've already tried number ${guessedNumber}`;
             } else{
                 if (guessedNumber === generatedNumber){
+                    hiScore += 1;
                     document.getElementById('answer').innerHTML = 'correct!';
-                    document.querySelector('body').setAttribute('style', 'background-color: #417836;');
+                    colorChanger(1);
+                    document.getElementById('scoreItself').innerHTML = `${hiScore}`;
                     listOfGuessedNumbers.length = 0;
-                    lockInputs();
+                    hiScore = 0;
+                    inputLockUnlock(true);
                     createPlayAgainButton();
                 } else if (guessedNumber < generatedNumber) {
                     document.getElementById('answer').innerHTML = 'incorrect, try higher';
                     listOfGuessedNumbers.push(guessedNumber);
+                    hiScore += 1;
                 } else if (guessedNumber > generatedNumber) {
                     document.getElementById('answer').innerHTML = 'incorrect, try lower';
                     listOfGuessedNumbers.push(guessedNumber);
+                    hiScore += 1;
                 } else {
-                    alert('Something unexpected happened! Please refresh the site.')
+                    alert('Something unexpected happened! Please refresh the site.');
                 }
                 const numberInputField = document.getElementById('submitAnswer');
                 numberInputField.addEventListener('click', function clearField(event) {
@@ -37,9 +42,28 @@ function answerChecking(){
     return;
 }
 
-function lockInputs(){
-    document.getElementById('numberInput').setAttributeNode(document.createAttribute('disabled'));
-    document.getElementById('submitAnswer').setAttributeNode(document.createAttribute('disabled'));
+function colorChanger(changeTo){
+    if (changeTo == 1){
+        document.querySelector('body').setAttribute('style', 'background-color: #417836;');
+        document.querySelector('footer').setAttribute('style', 'background-color: #417836;');
+    } else if (changeTo == 0){
+        document.querySelector('body').setAttribute('style', 'background-color: #708090;');
+        document.querySelector('footer').setAttribute('style', 'background-color: #708090;');
+    } else {
+        alert('Something unexpected happened! Please refresh the site.');
+    }
+}
+
+function inputLockUnlock(lock){
+    if (lock == true){
+        document.getElementById('numberInput').setAttributeNode(document.createAttribute('disabled'));
+        document.getElementById('submitAnswer').setAttributeNode(document.createAttribute('disabled'));
+    } else if (lock == false){
+        document.getElementById('numberInput').removeAttribute('disabled');
+        document.getElementById('submitAnswer').removeAttribute('disabled');
+    } else {
+        alert('Something unexpected happened! Please refresh the site.');
+    }
 }
 
 function createPlayAgainButton(){
@@ -56,9 +80,8 @@ function createPlayAgainButton(){
     playAgain.addEventListener('click', function ifClickedPlayAgain(event){
         playAgain.remove();
         document.getElementById('answer').innerHTML = '';
-        document.getElementById('numberInput').removeAttribute('disabled');
-        document.getElementById('submitAnswer').removeAttribute('disabled');
-        document.querySelector('body').setAttribute('style', 'background-color: #708090;');
+        colorChanger(0);
+        inputLockUnlock(false);
         globalThis.generatedNumber = generatingNumber();
     });
     return;
@@ -81,19 +104,15 @@ function playButton(){
         const createButtonDestination = document.createElement('span');
         const breakLine1 = document.createElement('br');
         const breakLine2 = document.createElement('br');
+        let elementList = [createAnswerTitle, breakLine1, createAnswerPlaceholder, breakLine2, createButtonDestination]
         createAnswerTitle.setAttribute('id', 'answerTitle');
         createAnswerPlaceholder.setAttribute('id', 'answer');
         createButtonDestination.setAttribute('id', 'buttonDestination');
-        parentElement.append(createAnswerTitle);
-        parentElement.append(breakLine1);
-        parentElement.append(createAnswerPlaceholder);
-        parentElement.append(breakLine2);
-        parentElement.append(createButtonDestination);
+        elementList.forEach(element => parentElement.append(element));
         document.getElementById('answerTitle').innerHTML = 'Answer was...';
         document.getElementById('answer').innerHTML = '';
         document.getElementById('buttonDestination').innerHTML = '';
-        document.getElementById('numberInput').removeAttribute('disabled');
-        document.getElementById('submitAnswer').removeAttribute('disabled');
+        inputLockUnlock(false);
         globalThis.generatedNumber = generatingNumber();
     });
     return;
@@ -106,12 +125,8 @@ function generatingNumber(){
 }
 
 let listOfGuessedNumbers = [];
+let hiScore = 0;
 
 const siteWidth = 1280;
 let scale = screen.width /siteWidth;
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
-
-/* 
-dodac wynik rozgrywki + hiScore
-*/
